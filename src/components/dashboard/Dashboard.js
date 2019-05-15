927ff92fd5d9e549e15953a02dda4480217e2d7f
yuchin <yuchinpao@lionmail.com>
2019-05-15 18:02:36 +0800

    cloud functions

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,17 +4,17 @@ import ProjectList from "../projects/ProjectList";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
-import { Redirect } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
 
 class Dashboard extends Component {
   render() {
-    const { projects, auth } = this.props;
-    if (!auth.uid) return <Redirect to='/signin' />
+    const { projects, auth, notifications} = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />;
 
     return (
       <div className="Dashboard">
         <ProjectList projects={projects} />
-        <Notification />
+        <Notification notifications={notifications} />
       </div>
     );
   }
@@ -23,11 +23,12 @@ class Dashboard extends Component {
 const mapStateToPros = state => {
   return {
     projects: state.firestore.ordered.projects,
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications
   };
 };
 
 export default compose(
   connect(mapStateToPros),
-  firestoreConnect([{ collection: "projects" }])
+  firestoreConnect([{ collection: "projects" }, { collection: "notifications", limit: 3 }])
 )(Dashboard);
--- src/components/dashboard/Notification.js
+++ src/components/dashboard/Notification.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import moment from 'moment';
 
-const Notification = ()=>{
-return(
-  <div className="Notification">
-    this is Notification
-  </div>
-)
+const Notification = props => {
+  const { notifications } = props;
+  console.log("notifications", notifications);
+  return (
+    <div className="Notification">
+      <h2>NOTIFICATION BOARD</h2>
+      {notifications && notifications.map((item)=>{
+        return (
+            <div>
+            <li>{item.user}</li>
+            <li>{moment(item.time.toDate().fromNow)}</li>
+            </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Notification;
